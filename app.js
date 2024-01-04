f73af726134a7bf9c66ca055d48e7e867da400aa
Gabriel Rosa <88284946+GabrielGSRosa@users.noreply.github.com>
2024-01-04 16:08:01 -0300

    Update app.js
    Função de auto scroll

--- app.js
+++ app.js
@@ -13,3 +13,27 @@ function atualizarTextoComBaseNoTamanhoDaTela() {
   // Chama a função quando a página é carregada e redimensionada
   window.addEventListener("load", atualizarTextoComBaseNoTamanhoDaTela);
   window.addEventListener("resize", atualizarTextoComBaseNoTamanhoDaTela);
+
+
+// Auto rolagem
+
+// Adiciona um ouvinte de evento de clique a todos os botões com a classe 'botaoScroll'
+document.querySelectorAll('.botaoScroll').forEach(function(botao) {
+  botao.addEventListener('click', function() {
+    // Obtém o ID da seção correspondente ao botão clicado
+    const idDaSecao = botao.getAttribute('id').replace('botaoScroll', '');
+    
+    // Obtém a referência ao elemento da seção correspondente
+    const secaoCorrespondente = document.getElementById('secao' + idDaSecao);
+    
+    // Obtém a posição vertical da seção correspondente
+    const posicaoSecao = secaoCorrespondente.offsetTop;
+
+    // Rola a página até a posição da seção correspondente
+    window.scrollTo({
+      top: posicaoSecao,
+      behavior: 'smooth'
+    });
+  });
+});
+
