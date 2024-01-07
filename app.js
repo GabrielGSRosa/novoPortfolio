3bbdd06b76c5fa6412b846ab6844650d1dc9d839
Gabriel Rosa <88284946+GabrielGSRosa@users.noreply.github.com>
2024-01-07 14:01:10 -0300

    Update app.js
    Correção de bug

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
-function atualizarTextoComBaseNoTamanhoDaTela() {
-    var elementoTexto = document.getElementById("texto-dinamico");
-  
-    if (window.innerWidth <= 600) {
-      // Se a largura da tela for menor ou igual a 600 pixels (pode ajustar esse valor conforme necessário)
-      elementoTexto.textContent = "trabalhos";
-    } else {
-      // Se a largura da tela for maior que 600 pixels
-      elementoTexto.textContent = "Veja meus trabalhos";
-    }
-  }
-  
-  // Chama a função quando a página é carregada e redimensionada
-  window.addEventListener("load", atualizarTextoComBaseNoTamanhoDaTela);
-  window.addEventListener("resize", atualizarTextoComBaseNoTamanhoDaTela);
-
-
 // Auto rolagem
 
 // Adiciona um ouvinte de evento de clique a todos os botões com a classe 'botaoScroll'
